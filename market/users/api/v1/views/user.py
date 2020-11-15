037a92b84d95da3082629aa6d5a983aeb401c5a1
Vijay Pathak <helloblazevijay99@gmail.com>
2020-11-15 19:59:07 +0545

    updates email token verify

--- market/users/api/v1/views/user.py
+++ market/users/api/v1/views/user.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.tokens import default_token_generator
 from django.core.exceptions import ObjectDoesNotExist
+from django.utils.encoding import force_text
 from django.utils.http import urlsafe_base64_decode
 from rest_framework import exceptions
 from rest_framework.decorators import action
@@ -26,30 +27,33 @@ class UserViewSet(ListRetrieveSetMixin):
     serializer_class = UserSerializer
     permission_classes = [IsAdminPermission]
 
-    @action(methods=['post'], detail=False, url_path='verify-email')
-    def verify_email_token(self, request, uidb64, token):
-        token = request.data.get('token')
-
-        if not token:
-            raise exceptions.PermissionDenied("Token is required")
-
+    @action(detail=False, url_path='verify-email', permission_classes=[AllowAny])
+    def verify_email_token(self, request):
+        uid = request.GET.get('uid')
+        token = request.GET.get('token')
+        if not (token and uid):
+            raise exceptions.PermissionDenied("Token and uid is required.")
         try:
-            uid = urlsafe_base64_decode(uidb64).decode()
-            user = User.default_manager.get(pk=uid)
+            uid = force_text(urlsafe_base64_decode(uid))
+            user = self.get_queryset().get(pk=uid)
         except (ObjectDoesNotExist, ValueError, OverflowError, User.DoesNotExist):
-            raise exceptions.ValidationError("Token invalid or expired")
-
+            user = None
         if user is not None and default_token_generator.check_token(user, token):
+            if user.is_active is True:
+                raise exceptions.NotAcceptable("email is already verified.")
             user.is_active = True
             user.save()
+        else:
+            raise exceptions.ValidationError("Token invalid or expired")
         serializer = self.get_serializer(user)
-        return Response(serializer.data)
+        return Response({'message': 'email has been verified', 'user info': serializer.data})
 
-    @action(methods=['post'], detail=False, url_path='password-change', serializer_class=ResetPasswordSerializer,
-            permission_classes=[IsUserOwnerPermission])
-    def password_change(self, request):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        request.user.set_password(serializer.validated_data.get('new_password'))
-        request.user.save()
-        return Response({"message": "password has been reset with the new password."})
+
+@action(methods=['post'], detail=False, url_path='password-change', serializer_class=ResetPasswordSerializer,
+        permission_classes=[IsUserOwnerPermission])
+def password_change(self, request):
+    serializer = self.get_serializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    request.user.set_password(serializer.validated_data.get('new_password'))
+    request.user.save()
+    return Response({"message": "password has been reset with the new password."})
