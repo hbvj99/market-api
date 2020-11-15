2948eef3ffa8eb105aa7ee84fb64a58cd3f40d0d
Vijay Pathak <vpathak@outcodesoftware.com>
2020-11-15 12:00:26 +0545

    adds rating in comment

--- market/products/models/comment.py
+++ market/products/models/comment.py
@@ -6,10 +6,20 @@ from ..models.product import Product
 
 User = get_user_model()
 
+BLANK = None
+RATING = [
+    (1, 1),
+    (2, 2),
+    (3, 3),
+    (4, 4),
+    (5, 5)
+]
+
 
 class Comment(BaseModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Product, on_delete=models.CASCADE)
+    rating = models.IntegerField(choices=RATING, default=BLANK, blank=True, null=True)
     reply = models.TextField(max_length=300, blank=False)
 
     def __str__(self):
