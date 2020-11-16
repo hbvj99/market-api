dbd11a9a6de3c0d4f8a4669c7775efa15ad5cb27
Vijay Pathak <helloblazevijay99@gmail.com>
2020-11-16 23:05:21 +0545

    adds dynamic file path

--- market/commons/file_upload.py
+++ market/commons/file_upload.py
@@ -7,14 +7,19 @@ user_image_path = "user/image/"
 product_image_path = 'products/image/'
 
 
-def get_user_profile_upload_path(_, filename):
+def get_upload_path(_, filename):
     ext = filename.split('.')[-1]
     filename = "%s.%s" % (uuid.uuid4(), ext)
-    print(filename, user_image_path)
-    return os.path.join(user_image_path, filename)
+    from market.products.models import Product
+    from market.users.models import User
+    if isinstance(_, Product):
+        path = product_image_path
+    elif isinstance(_, User):
+        path = user_image_path
+    return os.path.join(path, filename)
 
 
-def validate_image_extension_size(file, extension, supported_extension, max_size_mb, *args, **kwargs):
+def validate_file_extension_size(file, extension, supported_extension, max_size_mb, *args, **kwargs):
     if extension not in supported_extension:
         raise exceptions.NotAcceptable({"message": "invalid file extension. i.e. jpg, jpeg, png are only supported."})
     file_size = file.size
