8b54112b97833a0b0471dc01395edd6c144e1012
Vijay Pathak <helloblazevijay99@gmail.com>
2020-11-17 12:23:20 +0545

    updates method to render html template

--- market/users/api/v1/views/user.py
+++ market/users/api/v1/views/user.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.tokens import default_token_generator
 from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import render
 from django.utils.encoding import force_text
 from django.utils.http import urlsafe_base64_decode
 from rest_framework import exceptions
@@ -45,8 +46,7 @@ class UserViewSet(ListRetrieveSetMixin):
             user.save()
         else:
             raise exceptions.ValidationError("Token invalid or expired")
-        serializer = self.get_serializer(user)
-        return Response({'message': 'email has been verified', 'user info': serializer.data})
+        return render(request, 'email/email_verify_success.html')
 
 
 @action(methods=['post'], detail=False, url_path='password-change', serializer_class=ResetPasswordSerializer,
